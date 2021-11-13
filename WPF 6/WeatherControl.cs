37c7c40ea0eacc13400819296a6e1ef21fd2f90a
Анна Белозерова <bel.denisovna@gmail.com>
2021-11-13 17:22:02 +0300

    Добавьте файлы проекта.

---
+++ WPF 6/WeatherControl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_6
+{
+    class WeatherControl: DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        private string windDirection;
+        private int windSpeed;
+        public enum precipitation
+        {
+            Sun =0,
+            Cloudy =1,
+            Rain =2,
+            Snow =3,
+        }
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty,value);
+        }
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperaature));
+        }
+
+        private static bool ValidateTemperaature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v>=-50 && v<=50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public string WindDirection
+        {
+            get => windDirection;
+            set =>windDirection = value;
+        }
+        public int WindSpeed
+        {
+            get => windSpeed;
+            set => windSpeed = value;
+        }
+    }
+}
